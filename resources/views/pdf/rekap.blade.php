dcba633a44fc9a2b52bd894b1f2b7fb16352ef37
LilAlamin <ronaldramadhani15@gmail.com>
2024-01-31 00:48:45 +0700

    nambahkan pdf foto
    belum bisa

--- resources/views/pdf/rekap.blade.php
+++ resources/views/pdf/rekap.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Rekap Pengaduan</title>
 </head>
+
 <body>
 
     <h2>Rekap Pengaduan</h2>
@@ -15,22 +17,33 @@
                 <th>Judul Pengaduan</th>
                 <th>Lokasi Kejadian</th>
                 <th>Deskripsi Pengaduan</th>
+                <th>Lampiran</th>
                 <!-- Tambahkan kolom-kolom lainnya sesuai kebutuhan -->
             </tr>
         </thead>
         <tbody>
-            @foreach($data as $item)
+            @foreach ($data as $item)
                 <tr>
                     <td>{{ $item->number }}</td>
                     <td>{{ $item->username }}</td>
                     <td>{{ $item->judul_pengaduan }}</td>
                     <td>{{ $item->lokasi_pengaduan }}</td>
                     <td>{{ $item->deskripsi_pengaduan }}</td>
+                    <td>
+                        <!-- Check if attachments is not null before looping -->
+                        @if ($item->attachments)
+                            @foreach ($item->attachments as $attachment)
+                            <img src="{{ url('/storage/Foto/' . $attachment->nama_file) }}" alt="Attachment">
+                            @endforeach
+                        @endif
+                    </td>
                     <!-- Tambahkan kolom-kolom lainnya sesuai kebutuhan -->
                 </tr>
             @endforeach
+
         </tbody>
     </table>
 
 </body>
+
 </html>
