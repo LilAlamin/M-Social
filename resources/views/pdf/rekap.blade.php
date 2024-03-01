98cca2c46199936f3244dd13c624a26b2a9487d8
LilAlamin <ronaldramadhani15@gmail.com>
2024-03-01 09:19:06 +0700

    Update pdf

--- resources/views/pdf/rekap.blade.php
+++ resources/views/pdf/rekap.blade.php
@@ -34,14 +34,19 @@
                     @if ($item->files->isNotEmpty())
                         @foreach ($item->files as $file)
                             <!-- Use $file->nama_file as needed -->
-                            <img src="{{ asset('Foto/' . $file->nama_file) }}">
+                            @php
+                                $path = public_path('storage/Foto/' . $file->nama_file);
+                                $file = file_get_contents($path);
+                                $base64 = base64_encode($file);
+                            @endphp
+                            <img src="data:image/png;base64,{{ $base64 }}" width="200" style="margin-top: 20px;">
                             @endforeach
                     @endif
                 </td>
                 <!-- Tambahkan kolom-kolom lainnya sesuai kebutuhan -->
             </tr>
         @endforeach
-        
+
 
         </tbody>
     </table>
